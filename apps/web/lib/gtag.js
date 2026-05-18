cc001545417addd210ad9526c6c6a3ecfbc4288b
Frogo-o <ralchev.gg@gmail.com>
2026-05-18 19:15:38 +0300

    Add Google Analytics event tracking

--- apps/web/lib/gtag.js
+++ apps/web/lib/gtag.js
@@ -3,8 +3,10 @@ export const GA_TRACKING_ID = "G-PM4GG76SWF";
 export function pageview(url) {
     if (typeof window === "undefined" || typeof window.gtag !== "function") return;
 
-    window.gtag("config", GA_TRACKING_ID, {
+    window.gtag("event", "page_view", {
+        page_location: window.location.href,
         page_path: url,
+        page_title: document.title,
     });
 }
 
